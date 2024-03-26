896565a9d5ae7c59da71a09ed818ca30b3c25f40
Klemensmandzaro <kamil.klemiato@wp.pl>
2024-03-26 11:43:34 +0100

    first commit

---
+++ obiektowka1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //zadanie_1.zrobzadanie();
+        //zadanie_2.zrobzadanie();
+        //zadanie_3.zrobzadanie();
+        //Zadanie_4.zrobzadanie();
+        Zadanie_5.zrobzadanie();
+    }
+}
